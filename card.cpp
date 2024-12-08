1a77ea2190855595ecd182e6a600293fb68b6661
Karson52 <leekarson52@gmail.com>
2024-12-08 17:24:45 -0600

    basic card class

--- card.cpp
+++ card.cpp
@@ -1,3 +1,26 @@
 #include "card.h"
 
-card::card() {}
+card::card() {
+    set_rank(-1);
+    set_color("none");
+}
+card::card(int rank, string color) {
+    set_rank(rank);
+    set_color(color);
+}
+
+int card::get_rank(){
+    return rank;
+}
+
+void card::set_rank(int rank){
+    this->rank = rank;
+}
+
+string card::get_color(){
+    return color;
+}
+
+void card::set_color(string color){
+    this->color = color;
+}
--- card.h
+++ card.h
@@ -1,10 +1,26 @@
 #ifndef CARD_H
 #define CARD_H
 
+using namespace std;
+#include <iostream>
+
 class card
 {
+
+private:
+    int rank;
+    string color;
 public:
     card();
+    card(int rank, string color);
+
+    int get_rank();
+    void set_rank(int rank);
+
+    string get_color();
+    void set_color(string color);
+
+
 };
 
 #endif // CARD_H
